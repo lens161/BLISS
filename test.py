2fef68cd973ee27a84463fe6a787f9864d6ba133
lennart <lens@itu.dk>
2025-02-06 11:11:13 +0100

    make list of tensors

--- test.py
+++ test.py
@@ -1,8 +1,39 @@
 import numpy as np
+import torch
 import sklearn.datasets
+from sklearn.model_selection import train_test_split as sklearn_train_test_split
 import h5py
 
 def generate_random_array(size: int, dimensions: int, centers: int):
+    '''
+    generate random training and test arrays
+    '''
     X, _ = sklearn.datasets.make_blobs(n_samples=size, n_features=dimensions, centers=centers, random_state=1)
-    #X_train, X_test = train_test_split(X, test_size=0.1)
-    #write_output(X_train, X_test, out_fn, distance)
+    train, test = train_test_split(X, test_size=0.1)
+    return train, test
+
+def train_test_split(X, test_size):
+    '''
+    split an nd.array into training and test data
+    '''
+    # dimension = dimension if not None else X.shape[1]
+    print(f"Splitting into train/test")
+    return sklearn_train_test_split(X, test_size=test_size, random_state=1)
+
+def make_train_test_tensors(train, test):
+    t_train = []
+    t_test = []
+    for vector in train:
+        t_train.append(torch.from_numpy(vector))
+    for vector in test:
+        t_test.append(torch.from_numpy(vector))
+    return t_train, t_test
+
+
+if __name__ == "__main__":
+    dimension = 128
+    train, test = generate_random_array(20, dimension, 1)
+    t_train, t_test = make_train_test_tensors(train, test)
+    print(t_train)
+    print(t_test)
+
