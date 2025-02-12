934d603059e036f3661937f69a06bace8950e39d
Nikki van Gasteren <nikkivangasteren@gmail.com>
2025-02-12 16:37:13 +0100

    Add more unit tests

--- test.py
+++ test.py
@@ -23,9 +23,13 @@ def train_test_split(X, test_size):
 
 class TestTrainMethods(unittest.TestCase):
 
-    mnist_train, mnist_test, mnist_nbrs = read_dataset("mnist-784-euclidean")
+    # mnist_train, mnist_test, mnist_nbrs = read_dataset("mnist-784-euclidean")
 
+    @unittest.skip("slow")
     def test_get_B(self):
+        '''
+        Test whether B value is correct according to our specification.
+        '''
         self.assertEqual(get_B(1000000), 1024) # SIFT
         self.assertEqual(get_B(60000), 256) # MNIST
         self.assertEqual(get_B(1000000000), 32768) # Billion-scale
@@ -33,19 +37,49 @@ class TestTrainMethods(unittest.TestCase):
         with self.assertRaises(Exception): # Empty dataset
             get_B(0)
 
+    @unittest.skip("slow")
     def test_read_dataset(self):
+        '''
+        Test whether reading in the dataset gives us an np array of the correct shape.
+        '''
         self.assertEqual(self.__class__.mnist_train.shape[0], 60000) # nr train items
         self.assertEqual(self.__class__.mnist_test.shape[0], 10000) # nr test items
         self.assertEqual(self.__class__.mnist_train.shape[1], 784) # dimensions
         self.assertEqual(self.__class__.mnist_test.shape[1], 784) # dimensions
         self.assertEqual(self.__class__.mnist_nbrs.shape[0], 10000) # nr test items
         self.assertEqual(self.__class__.mnist_nbrs.shape[1], 100) # nbrs per test item
-    
+
+    @unittest.skip("slow")
+    def test_get_nearest_neighbours_small(self):
+        '''
+        Test whether we find the correct nearest neighbours in a sample array.
+        '''
+        data = np.array([[1], [5], [88], [100], [125], [130], [132], [150], [273], [500]])
+        nbrs = get_nearest_neighbours_faiss_within_dataset(data, 2)
+        self.assertTrue(np.array_equal(nbrs[0], [1, 2]))
+        self.assertTrue(np.array_equal(nbrs[1], [0, 2]))
+        self.assertTrue(np.array_equal(nbrs[2], [3, 4]))
+        self.assertTrue(np.array_equal(nbrs[3], [2, 4]))
+        self.assertTrue(np.array_equal(nbrs[4], [5, 6]))
+        self.assertTrue(np.array_equal(nbrs[5], [6, 4]))
+        self.assertTrue(np.array_equal(nbrs[6], [5, 4]))
+        self.assertTrue(np.array_equal(nbrs[7], [6, 5]))
+        self.assertTrue(np.array_equal(nbrs[8], [7, 6]))
+        self.assertTrue(np.array_equal(nbrs[9], [8, 7]))
+
+    @unittest.skip("slow")    
     def test_get_nearest_neighbours(self):
+        '''
+        Test whether our nearest neighbour function finds the same nearest neighbours for test data in a real dataset.
+        '''
         nbrs_test = get_nearest_neighbours_faiss_in_different_dataset(self.__class__.mnist_train, self.__class__.mnist_test, 100)
         self.assertTrue(np.array_equal(np.sort(nbrs_test), np.sort(self.__class__.mnist_nbrs)))
-    
+        
+    @unittest.skip("slow")
     def test_get_nearest_neighbours_from_file(self):
+        '''
+        Test whether fetching neighbours from a file gives the same array as when computing them.
+        '''
         dataset_name = "mnist-784-euclidean"
         amount = 100
         # check that the file exists already, otherwise the test will just compute neighbours twice
@@ -53,8 +87,44 @@ class TestTrainMethods(unittest.TestCase):
         file_nbrs = get_train_nearest_neighbours_from_file(self.__class__.mnist_train, amount, dataset_name)
         fresh_nbrs = get_nearest_neighbours_faiss_within_dataset(self.__class__.mnist_train, amount)
         self.assertTrue(np.array_equal(np.sort(file_nbrs), np.sort(fresh_nbrs)))
-        
-    
+
+    def test_make_ground_truth_labels_small(self):
+        '''
+        Test whether ground truth labels are computed correctly.
+        '''
+        B = 3
+        data = np.array([[1], [5], [88], [100], [125], [130], [132], [150], [273], [500]])
+        index = np.array([0, 0, 2, 1, 0, 1, 1, 2, 2, 0])
+        nbrs = get_nearest_neighbours_faiss_within_dataset(data, 2)
+        labels = make_ground_truth_labels(B, nbrs, index)
+        self.assertTrue(np.array_equal(labels[0], [1, 0, 1]))
+        self.assertTrue(np.array_equal(labels[1], [1, 0, 1]))
+        self.assertTrue(np.array_equal(labels[2], [1, 1, 0]))
+        self.assertTrue(np.array_equal(labels[3], [1, 0, 1]))
+        self.assertTrue(np.array_equal(labels[4], [0, 1, 0]))
+        self.assertTrue(np.array_equal(labels[5], [1, 1, 0]))
+        self.assertTrue(np.array_equal(labels[6], [1, 1, 0]))
+        self.assertTrue(np.array_equal(labels[7], [0, 1, 0]))
+        self.assertTrue(np.array_equal(labels[8], [0, 1, 1]))
+        self.assertTrue(np.array_equal(labels[9], [0, 0, 1]))
+
+    def test_initial_bucket_assignment_small(self):
+        '''
+        Test whether initial assignment arrays are of the correct shape and that the assignment is somewhat fairly distributed.
+        '''
+        N = 10000
+        r = 1
+        B = get_B(N)
+        index, counts = assign_initital_buckets(N, r, B)
+        self.assertEqual(len(index), N)
+        self.assertEqual(len(counts), B)
+
+        expected_bucketsize = N/B
+        min_bucketsize = expected_bucketsize/2
+        max_bucketsize = expected_bucketsize*2
+        self.assertFalse((counts > max_bucketsize).any())
+        self.assertFalse((counts < min_bucketsize).any())
+           
     
 if __name__ == "__main__":
     unittest.main()
