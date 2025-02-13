e2cc2e82ecfbb3ed3b7b3ccb665492fe4e452095
Nikki van Gasteren <nikkivangasteren@gmail.com>
2025-02-13 10:38:40 +0100

    Refactor bucket reassignment

--- bliss.py
+++ bliss.py
@@ -41,7 +41,7 @@ class BLISS_NN(nn.Module):
 
         return x
 
-def train_model(model, dataset, index, iterations, k, bucket_sizes, neighbours, epochs_per_iteration, batch_size):
+def train_model(model, dataset, index, iterations, k, bucket_sizes, neighbours, epochs_per_iteration, batch_size, SIZE):
     criterion = nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     train_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=6)
@@ -66,10 +66,10 @@ def train_model(model, dataset, index, iterations, k, bucket_sizes, neighbours, 
             finish = time.time()
             elapsed = finish-start
             print(f"epoch {epoch} took {elapsed}")
-        reassign_buckets(model, dataset, k, index, bucket_sizes, neighbours, batch_size)
+        reassign_buckets(model, dataset, k, index, bucket_sizes, neighbours, batch_size, SIZE)
         print(f"index after iteration {i} = {index}")
 
-def reassign_buckets(model, dataset, k, index, bucket_sizes, neighbours, batch_size):
+def reassign_buckets(model, dataset, k, index, bucket_sizes, neighbours, batch_size, SIZE):
     model.eval()
     reassign_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=6)
     item_index = 0
@@ -82,27 +82,30 @@ def reassign_buckets(model, dataset, k, index, bucket_sizes, neighbours, batch_s
             # print(f"bucket probabilities: {bucket_probabilities}")
 
             for probability_vector in bucket_probabilities:
-                value, indices_of_topk_buckets = torch.topk(probability_vector, k)
-                # print(f"indices = {indices_of_topk_buckets}")
-                smallest_bucket = 0
-                smallest_bucket_size = SIZE
-                old_bucket = index[item_index]
-                for i in indices_of_topk_buckets:
-                    size = bucket_sizes[i]
-                    if size < smallest_bucket_size:
-                        smallest_bucket = i
-                        smallest_bucket_size = size
-            
-                index[item_index] = smallest_bucket
-                bucket_sizes[old_bucket] -=1
-                bucket_sizes[smallest_bucket] +=1
-                item_index+=1        
+                reassign_vector_to_bucket(probability_vector, index, SIZE, bucket_sizes, k, item_index)
+                     
     finish = time.time()
     elapsed = finish - start
     print(f"reassigning took {elapsed}")
     new_labels = make_ground_truth_labels(B, neighbours, index)
     dataset.labels = new_labels
-    
+
+def reassign_vector_to_bucket(probability_vector, index, SIZE, bucket_sizes, k, item_index):
+    value, indices_of_topk_buckets = torch.topk(probability_vector, k)
+    # print(f"indices = {indices_of_topk_buckets}")
+    smallest_bucket = 0
+    smallest_bucket_size = SIZE
+    old_bucket = index[item_index]
+    for i in indices_of_topk_buckets:
+        size = bucket_sizes[i]
+        if size < smallest_bucket_size:
+            smallest_bucket = i
+            smallest_bucket_size = size
+
+    index[item_index] = smallest_bucket
+    bucket_sizes[old_bucket] -=1
+    bucket_sizes[smallest_bucket] +=1
+    item_index+=1   
             
 def assign_initital_buckets(train_size, r, B):
     '''
@@ -132,17 +135,17 @@ def make_ground_truth_labels(B, neighbours, index):
     return labels
 
 if __name__ == "__main__":
-    BATCH_SIZE = 64
+    BATCH_SIZE = 256
     EPOCHS = 5
-    ITERATIONS = 5
+    ITERATIONS = 20
     K = 2
     NR_NEIGHBOURS = 100
     device = get_best_device()
     # device = "cpu"
     print("Using device:", device)
 
-    # dataset_name = "sift-128-euclidean"
-    dataset_name = "mnist-784-euclidean"
+    dataset_name = "sift-128-euclidean"
+    # dataset_name = "mnist-784-euclidean"
     train, _, _ = read_dataset(dataset_name)
     print("training data_________________________")
     print(np.shape(train))
@@ -155,7 +158,7 @@ if __name__ == "__main__":
     index, counts = assign_initital_buckets(len(train), R, B)
 
     print("looking for true neighbours")
-    neighbours = get_nearest_neighbours_old(train, 20)
+    neighbours = get_train_nearest_neighbours_from_file(train, NR_NEIGHBOURS, dataset_name)
     print(neighbours)
 
     print("making ground truth labels")
@@ -166,7 +169,7 @@ if __name__ == "__main__":
 
     model.to(device)
     print("training model")
-    train_model(model, dataset, index, ITERATIONS, K, counts, neighbours, EPOCHS, BATCH_SIZE)
+    train_model(model, dataset, index, ITERATIONS, K, counts, neighbours, EPOCHS, BATCH_SIZE, SIZE)
 
     np.set_printoptions(threshold=np.inf, suppress=True)
     print(counts)
