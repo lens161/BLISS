0e2c4623e5c5f99fa915ed892088f6a06778ba3b
lennart <lens@itu.dk>
2025-04-28 12:22:41 +0200

    fix reass_modes in train, add optimiser run for i, e, lr, bs in sift)

--- optimise3.py
+++ optimise3.py
@@ -8,9 +8,9 @@ DATASET = "sift-128-euclidean"
 DATASIZE = 1
 EXP_NAME = "optimise"
 
-def optimise_load_balance(bucket_size, learning_rate, batch_size, trial):
+def optimise_load_balance(bucket_size, learning_rate, batch_size, epochs, iterations, trial):
     conf = Config(dataset_name=DATASET, b=bucket_size, lr=learning_rate,
-                  batch_size=batch_size, datasize=DATASIZE)
+                  batch_size=batch_size, epochs=epochs, iterations=iterations, datasize=DATASIZE)
     
     try:
         _, _, _, _, _, norm_entropy, _, _, _ = run_bliss(conf, mode="build", experiment_name=EXP_NAME, trial=trial)
@@ -34,11 +34,13 @@ def optimise_m(bucket_size, learning_rate, batch_size, m, trial):
 
 def objective(trial):
     bucket_size = trial.suggest_categorical('B', [4096])
-    learning_rate = trial.suggest_categorical('lr', [0.0005, 0.004, 0.005, 0.007, 0.01])
-    batch_size = trial.suggest_categorical('batch_size', [1000, 2000, 3000, 4000, 5000])
+    learning_rate = trial.suggest_categorical('lr', [0.0005, 0.001, 0.01])
+    batch_size = trial.suggest_categorical('batch_size', [3000, 4000, 5000])
+    epochs = trial.suggest_categorical('e', [3, 4, 5, 6])
+    iterations = trial.suggest_categorical('i', [3, 4, 5])
     # m = trial.suggest_categorical('m', [5, 25])
 
-    load_balance = optimise_load_balance(bucket_size, learning_rate, batch_size, trial)
+    load_balance = optimise_load_balance(bucket_size, learning_rate, batch_size, epochs, iterations, trial)
     return load_balance
 
 def create_study_load_balance(name):
@@ -65,7 +67,7 @@ def create_study_load_balance(name):
 #     study.optimize(objective, n_trials=10)
 
 if __name__ == '__main__':
-    STUDY_NAME = "find_b_bs_lr_for_sift"
+    STUDY_NAME = "find_bs_lr_e_i_for_sift"
 
     optuna.study.delete_study(STUDY_NAME, "sqlite:///opt_bliss.db")
 
